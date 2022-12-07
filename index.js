8981437c06c9585b0eafefd2cbce1f61197e19ca
James Addison <71357672+siliconjungle@users.noreply.github.com>
2022-12-07 11:37:04 +1100

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ export const set = (bitset, i) => {
   bitset[elementIndex] = bitset[elementIndex] | (1 << bitIndex)
 }
 
+export const replaceBits = (bitset, startIndex, value, length) => {
+  for (let i = 0; i < length; i++) {
+    if ((value >> i) & 1) {
+      set(bitset, startIndex + i)
+    } else {
+      clear(bitset, startIndex + i)
+    }
+  }
+
+  return bitset
+}
+
 export const clear = (bitset, i) => {
   const elementIndex = Math.floor(i / 32)
   const bitIndex = i % 32
